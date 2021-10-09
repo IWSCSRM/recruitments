cc2497a7c971f4b63452c030295d78a19348ea9a
pranjay-poddar <pranjaypoddar@outlook.com>
2021-10-09 13:46:39 +0530

    added opt feature

--- src/app/profile-auth/profile-auth.component.ts
+++ src/app/profile-auth/profile-auth.component.ts
@@ -8,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 export class ProfileAuthComponent implements OnInit {
   // passwordPattern="(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{8,}";
   emailPattern="^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$";
-  constructor() { }
+   unhide = "unhide";
+   hide = "hide"
+  toggle(){
+    if(this.unhide){
+      this.unhide = "hide";
+    }
+    else{
+      this.unhide = "hide";
+    }
+    if(this.hide){
+      this.hide = "unhide";
+    }
+    else{
+      this.hide = "hide";
+    }
 
-  ngOnInit(): void {
   }
-
+ 
+  constructor() { }
+ 
+  ngOnInit(): void {
+    
+  }
+ 
 }
