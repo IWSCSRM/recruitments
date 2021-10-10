99cd5c293c11597e7c53e69a103eb80cab5828b5
shreyanshp25 <shreyansh2511@gmail.com>
2021-10-10 23:23:03 +0530

    added admin login page

---
+++ src/app/admin-login/admin-login.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-login',
+  templateUrl: './admin-login.component.html',
+  styleUrls: ['./admin-login.component.scss']
+})
+export class AdminLoginComponent implements OnInit {
+  emailPattern="^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
